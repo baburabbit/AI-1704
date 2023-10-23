15a7334b8575b36a5248718ea0619ee74715ad30
Babu Nagarajan <112308559+baburabbit@users.noreply.github.com>
2023-10-23 21:53:18 +0530

    Update 8 queen.py

--- 8 queen.py
+++ 8 queen.py
@@ -1,55 +1,27 @@
-def is_safe(board, row, col):
-    # Check if there is a queen in the same row
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Check if there is a queen in the upper diagonal on the left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check if there is a queen in the lower diagonal on the left side
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-def solve_queens(board, col):
-    # If all queens are placed, return True
-    if col >= len(board):
-        return True
-
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            # Place queen
-            board[i][col] = 1
-
-            # Recur to place the rest of the queens
-            if solve_queens(board, col + 1):
-                return True
-
-            # If placing the queen in the current position doesn't lead to a solution, backtrack
-            board[i][col] = 0
-
-    # If no row is found where the queen can be placed in this column, return False
+print ("enter the number")
+N = int(input())
+list= [[0]*N for _ in range(N)]
+def attack(i, j):
+    for k in range(0,N):
+        if list[i][k]==1 or list[k][j]==1:
+            return True
+    for k in range(0,N):
+        for l in range(0,N):
+            if (k+l==i+j) or (k-l==i-j):
+                if list[k][l]==1:
+                    return True
     return False
-
-def print_solution(board):
-    for row in board:
-        print(" ".join("Q" if cell == 1 else "." for cell in row))
-    print()
-
-def solve_8_queens():
-    board = [[0 for _ in range(8)] for _ in range(8)]
-
-    if solve_queens(board, 0):
-        print("Solution found:")
-        print_solution(board)
-    else:
-        print("No solution found.")
-
-if __name__ == "__main__":
-    solve_8_queens()
-
+def N_queens(n):
+    if n==0:
+        return True
+    for i in range(0,N):
+        for j in range(0,N):
+            if (not(attack(i,j))) and (list[i][j]!=1):
+                list[i][j] = 1
+                if N_queens(n-1)==True:
+                    return True
+                list[i][j] = 0
+    return False
+N_queens(N)
+for i in list:
+    print (i)
